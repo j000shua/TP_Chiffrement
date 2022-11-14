2da27264be0fbcb7eb90ab69cc404dfef5e40366
LINGUET Joshua <jlinguet>
2022-11-14 11:12:03 +0100

    chiffrement

--- src/TranspositionInverse.java
+++ src/TranspositionInverse.java
@@ -9,11 +9,13 @@ public class TranspositionInverse implements ProtocoleChiffrement
 	@Override
 	public String chiffre(String message) 
 	{
+		return new StringBuffer(message).reverse().toString();
 	}
 
 	@Override
 	public String dechiffre(String message) 
 	{
+		return new StringBuffer(message).reverse().toString();
 	}
 	
 }
